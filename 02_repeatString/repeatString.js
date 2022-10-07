aff5b6b8a5d0c71dfbbf5b709ff2d61860b6738a
praisennamonu1 <praisennamonu@gmail.com>
2022-10-07 06:46:19 +0100

    passed repeatString tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let finalstring = " ";
+    let i = 0;
+    for (i = 0; i < num; i++) {
+        finalstring += string;
+    }
+    return finalstring;
 };
+console.log(repeatString('hey', 3));
 
 // Do not edit below this line
 module.exports = repeatString;
