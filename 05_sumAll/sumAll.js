dc8f3ac146b9319f90e1e07ac48dcd9636a1f8cc
praisennamonu1 <praisennamonu@gmail.com>
2022-10-07 06:47:25 +0100

    passed sumAll tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,29 @@
-const sumAll = function() {
+const sumAll = function(firstNum, lastNum) {
+    
+    let finalSum = 0;
+
+    if (lastNum < firstNum) {
+        for (let i = lastNum; i <= firstNum; i++) {
+            finalSum += i;
+        }
+        return finalSum;
+
+    } else if (firstNum !== NaN || lastNum !== NaN) {
+        return "ERROR";
+
+    } else if (firstNum < 0 || lastNum < 0) {
+        return "ERROR";
+
+    } else {
+        for (let i = firstNum; i <= lastNum; i++) {
+            finalSum += i;
+        }
+        return finalSum;
+    }
 
 };
+
+console.log(sumAll(-12, 4000));
 
 // Do not edit below this line
 module.exports = sumAll;
